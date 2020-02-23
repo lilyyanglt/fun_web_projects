0ba3347d363d22924002f9f3bc7fe1d16998831f
lily yang <lya702@hotmail.com>
2020-02-22 16:50:49 -0800

    Changed door images

--- guessing_game/script.js
+++ guessing_game/script.js
@@ -1,7 +1,7 @@
 
-const redDoor = document.querySelector("#btn-orange-img")
-const greyDoor = document.querySelector("#btn-grey-img")
-const cyanDoor = document.querySelector("#btn-cyan-img")
+const door1 = document.querySelector("#btn-orange-img")
+const door2 = document.querySelector("#btn-grey-img")
+const door3 = document.querySelector("#btn-cyan-img")
 const reloadButton = document.querySelector("#reload")
 const output = document.querySelector("#output")
 
@@ -14,16 +14,16 @@ for (let i = 0; i < images.length; i++) {
   images[randomIndex] = temp
 }
 
-redDoor.onclick = function(e) {
-  changeImage(redDoor, 0)
+door1.onclick = function(e) {
+  changeImage(door1, 0)
 }
 
-greyDoor.onclick = function(e) {
-  changeImage(greyDoor, 1)
+door2.onclick = function(e) {
+  changeImage(door2, 1)
 }
 
-cyanDoor.onclick = function(e) {
-  changeImage(cyanDoor, 2)
+door3.onclick = function(e) {
+  changeImage(door3, 2)
 }
 
 reloadButton.onclick = function(e) {
