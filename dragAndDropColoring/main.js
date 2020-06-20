05c05e5185d1f285300c464698543ecd5c266208
Lily <lilyyanglt@gmail.com>
2020-06-19 22:17:56 -0700

    Drafted dragging functionality - green ball only

--- dragAndDropColoring/main.js
+++ dragAndDropColoring/main.js
@@ -1,2 +1,21 @@
-console.clear();
+const canvas = document.getElementById('painting_area');
+
+document.getElementById('color_1').ondragstart = function(e) {
+  console.log(e.target.outerHTML);
+  console.log(this);
+  e.dataTransfer.setData('application/my-app', e.target.id);
+  e.dataTransfer.dropEffect = 'copy';
+}
+
+canvas.ondragover = function(e) {
+  e.preventDefault();
+  e.dataTransfer.dropEffect = 'copy'; 
+}
+
+canvas.ondrop = function(e) {
+  e.preventDefault();
+  const data = e.dataTransfer.getData('application/my-app');
+  console.log(document.getElementById(data));
+  e.target.appendChild(document.getElementById(data));
+}
 
